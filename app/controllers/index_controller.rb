804511e5f824fe5db6f9720362af4ceec9785bcd
Pat Holland <email": null>
2017-01-20 15:54:02 -0800

    JS loads image at regular intervals -- trying to determine how to write to temp file

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,25 @@
+require 'tempfile'
+
+def iss_view_url(latitude, longitude)
+  'https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=42.000,42.000&zoom=8&size=640x400&key=' + ENV['GOOGLE_MAPS_API_KEY']
+end
+
 get '/' do
   erb :index, locals: {:url => Photo.last.url, position: Position.last}
+end
+
+iss_view_file = Tempfile.new('iss-view')
+open(iss_view_file.path, "wb") do |file|
+  open(iss_view_url(42.000,42.000)) do |content|
+    file.write(content)
+  end
+end
+
+get '/lake' do
+  p iss_view_file
+  p iss_view_file.path
+
+  send_file iss_view_file.path, type: 'image/jpeg', disposition: 'inline'
 end
 
 # PSEUDOCODE
--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ require 'erb'
 require 'rest-client'
 require 'json'
 require 'dotenv'
+Dotenv.load
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
