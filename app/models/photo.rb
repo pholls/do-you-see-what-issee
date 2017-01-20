6a981b715554a4cfb553e5469fd50046900731fd
Pat Holland <email": null>
2017-01-20 14:46:18 -0800

    Change column name on model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,11 @@
 class Photo < ActiveRecord::Base
-  belongs_to :location
+  belongs_to :position
 
   def self.get(position)
     latitude = position.latitude
     longitude = position.longitude
     url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=#{latitude},#{longitude}&zoom=8&size=640x400&key="
     url << ENV["GOOGLE_MAPS_API_KEY"]
-    Photo.create!(url: url)
+    Photo.create!(url: url, position: position)
   end
 end
