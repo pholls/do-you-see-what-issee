a1e0aaef528dd2cc80a7eac5190bb28a1089fa0e
Pat Holland <email": null>
2017-01-20 16:29:02 -0800

    With JS in application.js file, I don't have access to my ENV[API] variable

--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
-  // var live = true;
-  // function updateISSView() {
-  //   $.get('http://api.open-notify.org/iss-now.json').done(function(data) {
-  //     var $image = $('#iss-view');
-  //     var latitude = data["iss_position"]['latitude'];
-  //     var longitude = data["iss_position"]['longitude'];
-  //     var url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + latitude + ',' + longitude + "&zoom=8&size=640x400&key=" + "<%= ENV['GOOGLE_MAPS_API_KEY'] %>";
-  //     $image.attr('src', url);
-  //   })
-  //   if(live){
-  //      setTimeout(updateISSView, 5000);
-  //   }
-  // }
+  var live = true;
+  var interval = 10000;
+  function updateISSView() {
+    $.get('http://api.open-notify.org/iss-now.json').done(function(data) {
+      var $image = $('#iss-view');
+      var latitude = data["iss_position"]['latitude'];
+      var longitude = data["iss_position"]['longitude'];
+      var url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + latitude + ',' + longitude + "&zoom=8&size=640x400&key=" + "<%= ENV['GOOGLE_MAPS_API_KEY'] %>";
+      $image.attr('src', url);
+      updateCoordinates(latitude, longitude);
+    })
+  function updateCoordinates(latitude, longitude) {
+    $('#coordinates').text(latitude + ',' + longitude);
+  }
+    if(live){
+       setTimeout(updateISSView, interval);
+    }
+  }
 
-  // updateISSView();
+  updateISSView();
 
 });
