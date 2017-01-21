1c270a0acb8d4d1991075da1d02029fcaef5bf89
Pat Holland <email": null>
2017-01-20 16:24:33 -0800

    Write function in JS to update the coordinates of the ISS displayed on the page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // $.get(    ).done(function(data) {
-  //     console.log(data)
-  //   }).fail(function(data) {
-  //     console.log(data)
-  // })
+  // var live = true;
+  // function updateISSView() {
+  //   $.get('http://api.open-notify.org/iss-now.json').done(function(data) {
+  //     var $image = $('#iss-view');
+  //     var latitude = data["iss_position"]['latitude'];
+  //     var longitude = data["iss_position"]['longitude'];
+  //     var url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + latitude + ',' + longitude + "&zoom=8&size=640x400&key=" + "<%= ENV['GOOGLE_MAPS_API_KEY'] %>";
+  //     $image.attr('src', url);
+  //   })
+  //   if(live){
+  //      setTimeout(updateISSView, 5000);
+  //   }
+  // }
+
+  // updateISSView();
+
 });
