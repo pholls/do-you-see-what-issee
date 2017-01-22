046c0a9e902633af14c00551bca4044b7c8b89f5
Pat Holland <pholls@users.noreply.github.com>
2017-01-22 00:18:19 -0800

    Move script to application.js file

--- public/js/application.js
+++ public/js/application.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
-  // LINE 11: Do not have access to ENV['GOOGLE_MAPS_API'] variable here
-  // Need to figure out how to access that
+  var live = true;
+  var interval = 10000;
+  function updateISSView() {
+    $.get('http://api.open-notify.org/iss-now.json').done(function(data) {
+      var $image = $('#iss-view');
+      var latitude = data["iss_position"]['latitude'];
+      var longitude = data["iss_position"]['longitude'];
+      var url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + latitude + ',' + longitude + "&zoom=8&size=640x400&key=";
+      $image.attr('src', url);
+      updateCoordinates(latitude, longitude);
+      getAddress(latitude, longitude);
+    })
+  function updateCoordinates(latitude, longitude) {
+    $('#coordinates').text(latitude + ', ' + longitude);
+  }
+  function getAddress(latitude, longitude) {
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=';
+    $.get(url).done(function(data) {
+      $('#location').text(data['results'][1]['formatted_address']);
+    })
+  }
+  if(live){
+      setTimeout(updateISSView, interval);
+    }
+  }
 
-  // var live = true;
-  // var interval = 10000;
-  // function updateISSView() {
-  //   $.get('http://api.open-notify.org/iss-now.json').done(function(data) {
-  //     var $image = $('#iss-view');
-  //     var latitude = data["iss_position"]['latitude'];
-  //     var longitude = data["iss_position"]['longitude'];
-  //     var url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=" + latitude + ',' + longitude + "&zoom=8&size=640x400&key=" + "<%= ENV['GOOGLE_MAPS_API_KEY'] %>";
-  //     $image.attr('src', url);
-  //     updateCoordinates(latitude, longitude);
-  //   })
-  // function updateCoordinates(latitude, longitude) {
-  //   $('#coordinates').text(latitude + ',' + longitude);
-  // }
-  //   if(live){
-  //      setTimeout(updateISSView, interval);
-  //   }
-  // }
-
-  // updateISSView();
+  updateISSView();
 
 });
